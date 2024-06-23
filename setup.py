e985d487ea22d2c9227d3626f10c9a1cae82cc73
Danishjeet Singh <danishjeetsingh@gmail.com>
2024-06-23 13:45:57 -0400

    update: version 0.20

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="base36py",
-    version="0.1.0",
+    version="0.2.0",
     author="Danishjeet Singh",
     author_email="danishjeetsingh@gmail.com",
     description="A lightweight Python library for encoding and decoding numbers between base10 and base36.",
